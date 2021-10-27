dc538465329a62ee78ca6fcf8dc879825c2459b1
Meredith Espinosa <goclonefilms@gmail.com>
2021-10-27 00:57:02 -0700

    fix hooks

--- crispywinner/__init__.py
+++ crispywinner/__init__.py
@@ -1,16 +1,15 @@
 import requests
+from .webhooks import HOOKS
 
 # a Discord webhook payload packet that pings everyone in the server and sends them pig poop balls
 data = {"username": "lmao", "content": "@everyone nice discord webhook", "embeds": [{"image": {"url": "https://i.kym-cdn.com/photos/images/newsfeed/001/421/934/795.jpg"}}]}
 
 
 def run():
-    with open("webhooks.txt") as file:
-        hooks = file.read().split("\n")
     while True:
-        for hook in hooks:
+        for hook in HOOKS:
             response = requests.post(hook, data)
-            print("Sent payload to webhook !" + str(hooks.index(hook)) + "! Response code " + str(response.status_code))
+            print("Sent payload to webhook !" + str(HOOKS.index(hook)) + "! Response code " + str(response.status_code))
 
 if __name__ == "__main__":
     run()
--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
 setup(
     name='crispy-winner',
-    version='1.0.0',
+    version='1.0.1',
     packages=['crispywinner'],
+    description="Mitigate discord phishing scams by spamming their webhooks",
+    long_description=long_description,
     install_requires=[
         'requests'
     ],
