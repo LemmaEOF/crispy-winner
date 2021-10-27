7c12de60bd62b6a9a03b97a40c47c3b89e46c694
Meredith Espinosa <goclonefilms@gmail.com>
2021-10-27 00:50:31 -0700

    get PyPI publishing almost working

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='crispy-winner',
     version='1.0.0',
-    packages=['crispy-winner'],
+    packages=['crispywinner'],
     install_requires=[
         'requests'
     ],
     entry_points={
-        "console_scripts": ["crispy-winner = crispy-winner:run"]
+        "console_scripts": ["crispy-winner=crispywinner:run"]
     }
 )
