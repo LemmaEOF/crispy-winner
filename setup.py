cc688914c3de0e82702cc26b0f236a8e1e86526a
Meredith Espinosa <goclonefilms@gmail.com>
2021-10-27 01:15:56 -0700

    fix setup info, comment out killed webhook

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setup(
     name='crispy-winner',
-    version='1.0.1',
+    version='1.0.2',
     packages=['crispywinner'],
     description="Mitigate discord phishing scams by spamming their webhooks",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'requests'
     ],
